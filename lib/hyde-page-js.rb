gregdaynes/hyde-page-js

commit 6f05c353912bfc2b55bbae295288af09abe7ebb4

refactor!: complete rework
The initial implementation hacked up core Jekyll classes which would
cause rendering conflicts and unstable conditions.

A new approach fitting entirely within the Page Pre-Render hook, we can
achieve the same result in a simpler and more stable way.

lib/hyde-page-js.rb
@@ -1,11 +1,198 @@
+require 'terser'
+require 'digest'
+require 'jekyll'
+
+Jekyll::Hooks.register :pages, :pre_render do |page, payload|
+  Hyde::Page::Js.new(page).run
+end
+
 module Hyde
   module Page
     class Js
-      VERSION = "0.1.0"
+      VERSION = "0.2.0"
+    end
+
+    class GeneratedJsFile < Jekyll::StaticFile
+      attr_accessor :file_contents
+      attr_reader :generator
+
+      def initialize(site, dir, name)
+        @site = site
+        @dir = dir
+        @name = name
+        @relative_path = File.join(*[@dir, @name].compact)
+        @extname = File.extname(@name)
+        @type = @collection&.label&.to_sym
+        @generator = "hyde-page-js"
+      end
+
+      def write(dest)
+        dest_path = destination(dest)
+        return false if File.exist?(dest_path)
+
+        FileUtils.mkdir_p(File.dirname(dest_path))
+        FileUtils.rm(dest_path) if File.exist?(dest_path)
+
+        File.open(dest_path, "w") do |output_file|
+          output_file << file_contents
+        end
+
+        true
+      end
+    end
+
+    class Js
+      @@config = {
+        "source" => "assets/js",
+        "destination" => "assets/js",
+        "minify" => true,
+        "enable" => true,
+        "keep_files" => true,
+        "dev_mode" => false
+      }
+
+      def initialize(page)
+        @page = page
+        @site = page.site
+        @config = fetch_config
+
+        if keep_files? && !dev_mode?
+          @site.config.fetch("keep_files").push(destination)
+        end
+      end
+
+      def run
+        js = fetch_js(@page)
+        layout = fetch_layout(fetch_layout_name(@page))
+        results = parent_layout_js(layout, js).reverse
+
+        data = concatenate_files(results)
+        return if data == ""
+
+        data = minify(data)
+        return if data == ""
+
+        generated_file = generate_file(results, data)
+
+        # file already exists, so skip writing out the data to disk
+        return unless @site.static_files.find { |static_file| static_file.name == generated_file.name }.nil?
+
+        # place file data into the new file
+        generated_file.file_contents = data
+
+        # assign static file to list for jekyll to render
+        @site.static_files << generated_file
+
+        # assign to site.data.js_files for liquid output
+        add_to_urls(generated_file.url)
+      end
+
+      private
+
+      def add_to_urls(url)
+        @site.data['js_files'] ||= []
+        @site.data['js_files'].push(url)
+      end
+
+      def fetch_config
+        @@config.merge(@site.config.fetch("hyde_page_js", {}))
+      end
+
+      def keep_files?
+        @config.fetch("keep_files") == true
+      end
+
+      def dev_mode?
+        @config.fetch("dev_mode") == true
+      end
+
+      def minify?
+        @config.fetch("minify") == true
+      end
+
+      def destination
+        @config.fetch("destination")
+      end
+
+      def source
+        @config.fetch("source")
+      end
+
+      def qualified_source
+        File.join(*[@site.source, source].compact)
+      end
+
+      def fetch_layout_name(obj_with_data, default = nil)
+        obj_with_data.data.fetch('layout', default)
+      end
+
+      def fetch_js(obj_with_data, default = [])
+        obj_with_data.data.fetch('js', []).reverse
+      end
+
+      def fetch_layout(layout_name, default = nil)
+        @site.layouts.fetch(layout_name, default)
+      end
+
+      def mangle?
+        if dev_mode?
+          false
+        elsif minify?
+          true
+        end
+      end
+
+      def parent_layout_js(layout, js)
+        if layout.nil?
+          return js.uniq.compact
+        end
+
+        layout_name = fetch_layout_name(layout)
+        parent_layout = fetch_layout(layout_name)
+        js = js.concat(fetch_js(layout))
+
+        parent_layout_js(parent_layout, js)
+      end
+
+      def concatenate_files(files, data = [])
+        files.each do |file_name|
+          # tmp page required to handle anything with frontmatter/yaml header
+          tmp_page = Jekyll::PageWithoutAFile.new(@site, nil, source, file_name)
+          path = File.join([qualified_source, file_name])
+
+          begin
+            tmp_page.content = File.read(path)
+            data.push(Jekyll::Renderer.new(@site, tmp_page).run)
+          rescue
+            Jekyll.logger.warn("Page JS Warning:", "Unable to find #{path}")
+          end
+        end
+
+        data.join("\n")
+      end
+
+      def minify(data)
+        converter_config = {mangle: mangle?, output: {comments: :copyright}}
+        js_converter = Terser.new(converter_config)
+        js_converter.compile(data)
+      end
+
+      def generate_file(files, data)
+        file_name = generate_file_name(files, data)
+        Hyde::Page::GeneratedJsFile.new(@site, source, file_name)
+      end
+
+      def generate_file_name(files, data, prefix: nil)
+        file_names = [prefix]
+
+        if dev_mode?
+          files.each { |file| file_names.push(file.gsub(".js", "")) }
+        end
+
+        file_names.push(Digest::MD5.hexdigest(data)[0, 6])
+
+        file_names.compact.join("-") + ".js"
+      end
     end
   end
 end
-
-require_relative "hyde/renderer"
-require_relative "hyde/page/js"
-require_relative "hyde/page/generated_page_js_file"

example/Gemfile
@@ -16,7 +16,7 @@ gem "minima", "~> 2.5"
 # If you have any plugins, put them here!
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.12"
-  gem "hyde-page-js", "~> 0.1"
+  gem "hyde-page-js", "~> 0.2.0", :path => "../"
 end
 
 # Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem
@@ -32,3 +32,5 @@ gem "wdm", "~> 0.1.1", platforms: [:mingw, :x64_mingw, :mswin]
 # Lock `http_parser.rb` gem to `v0.6.x` on JRuby builds since newer versions of the gem
 # do not have a Java counterpart.
 gem "http_parser.rb", "~> 0.6.0", platforms: [:jruby]
+
+gem "terser", "~> 1.1"

example/_layouts/base.html
@@ -13,10 +13,9 @@ js:
       </div>
     </main>
 
-    {%- for file in page.js_files -%}
+    {%- for file in site.data.js_files -%}
     <script src="{{ file | prepend: '/' | prepend: site.baseurl }}"></script>
     {%- endfor %}
 
   </body>
-
 </html>

hyde-page-js.gemspec
@@ -1,4 +1,4 @@
-require File.expand_path("../lib/hyde-page-js.rb", __FILE__)
+require File.expand_path("./lib/hyde-page-js.rb")
 
 Gem::Specification.new do |s|
   s.name = "hyde-page-js"
@@ -14,5 +14,5 @@ Gem::Specification.new do |s|
   s.require_path = "lib"
 
   s.add_development_dependency "jekyll", ">= 4.0", "< 5.0"
-  s.add_development_dependency "terser", "~> 1.1"
+  s.add_dependency "terser", "~> 1.1"
 end

lib/hyde/page/generated_page_js_file.rb
@@ -1,35 +1,0 @@
-module Hyde
-  module Page
-    # Alternative class for jekyll's static files
-    # this allows the creation of files without a source file
-
-    class GeneratedPageJsFile < Jekyll::StaticFile
-      attr_accessor :file_contents
-      attr_reader :generator
-
-      def initialize(site, dir, name)
-        @site = site
-        @dir = dir
-        @name = name
-        @relative_path = File.join(*[@dir, @name].compact)
-        @extname = File.extname(@name)
-        @type = @collection&.label&.to_sym
-        @generator = "hyde-page-js"
-      end
-
-      def write(dest)
-        dest_path = destination(dest)
-        return false if File.exist?(dest_path)
-
-        FileUtils.mkdir_p(File.dirname(dest_path))
-        FileUtils.rm(dest_path) if File.exist?(dest_path)
-
-        File.open(dest_path, "w") do |output_file|
-          output_file << file_contents
-        end
-
-        true
-      end
-    end
-  end
-end

lib/hyde/page/js.rb
@@ -1,131 +1,0 @@
-require "jekyll"
-require "terser"
-require "digest"
-require_relative "generated_page_js_file"
-
-module Hyde
-  module Page
-    class Js
-      @@config = {
-        "asset_path" => "assets/js",
-        "file_output_path" => "assets/js",
-        "js_minify" => true,
-        "enable" => true,
-        "keep_files" => true,
-        "dev_mode" => false
-      }
-
-      def initialize(layout, info)
-        @site = info[:registers][:site]
-        @page = info[:registers][:page]
-        @data = layout.data
-        @config = @@config.merge(@site.config.dig("hyde_page_js") || {})
-        @qualified_asset_path = File.join(*[@site.source, @config["asset_path"]].compact)
-
-        if config("keep_files") == true && config("dev_mode") == false
-          @site.config["keep_files"].push(config("file_output_path"))
-        end
-
-        @page["js"] = if @page["js"].nil?
-          []
-        else
-          [@page["js"]]
-        end
-      end
-
-      def generate
-        return unless config("enable") == true
-        @page["js"].unshift(@data["js"])
-
-        file_groups = flatten_group(@page["js"])
-
-        js_files = []
-
-        file_groups.each do |files|
-          break if !files&.length
-
-          data = concatenate_files(files.compact)
-          data = minify(data)
-          next if data == ""
-
-          file = generate_file(files, data)
-          js_files << file.url
-
-          # file already exists, so skip writing out the data to disk
-          next if @site.static_files.find { |x| x.name == file.name }
-
-          # place file data into the new file
-          file.file_contents = data
-
-          # assign static file to list for jekyll to render
-          @site.static_files << file
-        end
-
-        # the recursive nature of this will sometimes have duplicate js files
-        @page["js_files"] = js_files.uniq
-      end
-
-      private
-
-      def config(*)
-        @config.dig(*)
-      end
-
-      def flatten_group(arr, acc = [])
-        return [arr] if !arr.last.is_a?(Array)
-
-        acc += [arr.first]
-        acc + flatten_group(arr.last)
-      end
-
-      def concatenate_files(files, data = "")
-        files.each do |file|
-          # tmp page required to handle anything with frontmatter/yaml header
-          tmp_page = Jekyll::PageWithoutAFile.new(@site, nil, config("asset_path"), file)
-          path = File.join([@qualified_asset_path, file])
-
-          begin
-            file_contents = File.read(path)
-            tmp_page.content = file_contents
-            # original jekyll renderer, not our modified version
-            data << Jekyll::Renderer.new(@site, tmp_page).run
-          rescue
-            Jekyll.logger.warn("Page JS Warning:", "Unable to find #{path}")
-          end
-        end
-
-        data
-      end
-
-      def minify(data)
-        mangle = if config("dev_mode") == true
-          false
-        else
-          !(config("js_minify") == false)
-        end
-
-        converter_config = {mangle: mangle, output: {comments: :copyright}}
-        js_converter = Terser.new(converter_config)
-        js_converter.compile(data)
-      end
-
-      def generate_file(files, data)
-        file_name = generate_file_name(files, data)
-
-        Hyde::Page::GeneratedPageJsFile.new(@site, config("asset_path"), file_name)
-      end
-
-      def generate_file_name(files, data, prefix: nil)
-        file_names = [prefix]
-
-        if config("dev_mode")
-          files.each { |file| file_names.push(file.gsub(".js", "")) }
-        end
-
-        file_names.push(Digest::MD5.hexdigest(data)[0, 6])
-
-        file_names.compact.join("-") + ".js"
-      end
-    end
-  end
-end

lib/hyde/renderer.rb
@@ -1,18 +1,0 @@
-module Jekyll
-  class Renderer
-    def render_layout(output, layout, info)
-      Hyde::Page::Js.new(layout, info).generate
-
-      # TODO Would be nice to call super here instead of cloned logic from Jekyll internals
-      payload["content"] = output
-      payload["layout"] = Utils.deep_merge_hashes(layout.data, payload["layout"] || {})
-
-      render_liquid(
-        layout.content,
-        payload,
-        info,
-        layout.path
-      )
-    end
-  end
-end

commit 229af4bee74c2cc9e9cb3d32593db6d8d91369e7

fix!: regression that combined all css files
The refactoring in 6f05c353 introduced a regression where instead of
combining js on a per-layout bases and exposing those to the page, the
change combined all the js per page and put it on every page.

This change should restore original functionality where a nested layout
page eg:
- base.html: js-a, js-b
  - home.html: js-c

Would add an array to the `page` data containing 2 entries
1. `assets/js/js-a-js-b.js`
2. `assets/js/js-c`

lib/hyde-page-js.rb
@@ -2,8 +2,8 @@ require 'terser'
 require 'digest'
 require 'jekyll'
 
-Jekyll::Hooks.register :pages, :pre_render do |page, payload|
-  Hyde::Page::Js.new(page).run
+Jekyll::Hooks.register :pages, :post_init do |page, payload|
+  Hyde::Page::Js.new(page).run if page.instance_of? Jekyll::Page
 end
 
 module Hyde
@@ -65,34 +65,42 @@ module Hyde
       def run
         js = fetch_js(@page)
         layout = fetch_layout(fetch_layout_name(@page))
-        results = parent_layout_js(layout, js).reverse
-        return if results.empty?
+        js_groups = parent_layout_js(layout, js).reverse
+        return if js_groups.empty?
 
-        lookup_name = names_to_key(results)
-        return unless @site.data['_hyde_pages_cache'].fetch(lookup_name, nil).nil?
+        for group in js_groups
+          lookup_name = names_to_key(group)
+          cache_entry = @site.data['_hyde_pages_cache'].fetch(lookup_name, nil)
 
-        data = concatenate_files(results)
-        return if data == ""
+          if cache_entry.nil?
+            data = concatenate_files(group)
+            return if data == ""
 
-        data = minify(data)
-        return if data == ""
+            data = minify(data)
+            return if data == ""
 
-        generated_file = generate_file(results, data)
+            generated_file = generate_file(group, data)
 
-        # file already exists, so skip writing out the data to disk
-        return unless @site.static_files.find { |static_file| static_file.name == generated_file.name }.nil?
+            # file already exists, so skip writing out the data to disk
+            return unless @site.static_files.find { |static_file| static_file.name == generated_file.name }.nil?
 
-        # place file data into the new file
-        generated_file.file_contents = data
+            # place file data into the new file
+            generated_file.file_contents = data
 
-        # assign static file to list for jekyll to render
-        @site.static_files << generated_file
+            # assign static file to list for jekyll to render
+            @site.static_files << generated_file
 
-        # assign to site.data.js_files for liquid output
-        add_to_urls(generated_file.url)
+            # add to cache
+            cache_entry = {
+              "url": generated_file.url,
+              "data": data
+            }
+            @site.data['_hyde_pages_cache'][lookup_name] = cache_entry
+          end
 
-        # add to cache
-        @site.data['_hyde_pages_cache'][lookup_name] = data
+          # assign to site.data.js_files for liquid output
+          add_to_urls(cache_entry.fetch(:url, nil)).compact
+        end
       end
 
       private
@@ -102,8 +110,8 @@ module Hyde
       end
 
       def add_to_urls(url)
-        @site.data['js_files'] ||= []
-        @site.data['js_files'].push(url)
+        @page.data['js_files'] ||= []
+        @page.data['js_files'].push(url)
       end
 
       def fetch_config
@@ -139,7 +147,7 @@ module Hyde
       end
 
       def fetch_js(obj_with_data, default = [])
-        obj_with_data.data.fetch('js', []).reverse
+        [obj_with_data.data.fetch('js', []).reverse]
       end
 
       def fetch_layout(layout_name, default = nil)
@@ -156,7 +164,7 @@ module Hyde
 
       def parent_layout_js(layout, js)
         if layout.nil?
-          return js.uniq.compact
+          return js
         end
 
         layout_name = fetch_layout_name(layout)